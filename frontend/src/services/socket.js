3dc7b4d0d2c818951b1c572a7383c4526f0cf8be
Ashish <ashishrana1805@gmail.com>
2025-06-17 13:19:58 +0530

    Update socket connection URL to production endpoint

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8000", {
+const socket = io("wss://ping-v1.onrender.com", {
   transports: ["websocket", "polling"],
   reconnection: true, // This is the default, but ensure it hasn't been disabled.
   reconnectionAttempts: Infinity,
