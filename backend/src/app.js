7e155c473d47f90fcd825799c22d32ce5d720af2
ashishrana18 <ashishrana1805@gmail.com>
2026-01-22 10:49:44 +0530

    import fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import errorHandler from "./middlewares/errorHandler.js";
-import ApiError from "./utils/ApiError.js";
+import { ApiError } from "./utils/ApiError.js";
 
 const app = express();
 app.use(
