9ed4f948d99ad6d0f2517f9d066f3b30cd5004b2
ashishrana18 <ashishrana1805@gmail.com>
2026-02-07 16:28:21 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,6 @@ import SecretChatPage from "./components/secretChat/SecretChatPage.jsx";
 import NoPageFound from "./components/chat/noPageFound.jsx";
 
 function App() {
-  //hello
   return (
     <BrowserRouter>
       <AuthProvider>
